55613cc92bac8309b9d352661b22e2cac05a1331
JTWebMan <jtwebman@gmail.com>
2011-10-03 09:04:11 -0600

    Made fibonacci function not block while calculating this will scale better

--- index.js
+++ index.js
@@ -1,13 +1,21 @@
 module.exports = function (n) {
-	function fibonacci () {
-		if (n < 2) {
-			return 1;
-		} else {
-			return fibonacci(n-2) + fibonacci(n-1);
-		}
-	}
+	function fibonacci(n, done) {
+    if (n < 2) {
+      done(1);
+    } else {
+      process.nextTick(function() {
+        fibonacci(n - 2, function(val1) {
+          fibonacci(n - 1, function(val2) {
+            done(val1 + val2);
+          });
+        });
+      });
+    }
+  }
 	return function (req, res, next) {
-		req.fibonacci = fibonacci();
-		next();
+    fibonacci(n, function(value) {
+      req.fibonacci = value;
+      next();
+    });
 	}
 }
