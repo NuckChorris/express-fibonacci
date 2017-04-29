84b7dbb97856a42477f2b101cc92e14b67705f76
Recursive Lambda God <chris@circle.academy>
2017-04-29 15:32:24 -0400

    "Recursive Lambda Refactor V1"

--- index.js
+++ index.js
@@ -1,21 +1,21 @@
-module.exports = function (num) {
+module.exports = num => {
   function fibonacci(n, done) {
     if (n < 2) {
       done(1);
     } else {
-      process.nextTick(function() {
-        fibonacci(n - 2, function(val1) {
-          fibonacci(n - 1, function(val2) {
+      process.nextTick(() => {
+        fibonacci(n - 2, val1 => {
+          fibonacci(n - 1, val2 => {
             done(val1 + val2);
           });
         });
       });
     }
   }
-  return function (req, res, next) {
-    fibonacci(num, function(value) {
+  return (req, res, next) => {
+    fibonacci(num, value => {
       req.fibonacci = value;
       next();
     });
-	}
+	};
 }
